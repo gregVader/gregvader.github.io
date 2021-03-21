82f0a9a6ab423e2691bcf11f8707c57c477ee691
gregVader <kgerg13@gmail.com>
2021-03-21 20:34:23 +0100

    Travis bla bla

---
+++ script/cibuild.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# skip if build is triggered by pull request
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# cleanup "_site"
+#rm -rf _site
+#mkdir _site
+
+# clone remote repo to "_site"
+git clone https://${GH_TOKEN}@github.com/gregVader/gregvader.github.io.git #--branch gh-pages _site
+
+# build with Jekyll into "_site"
+gem install bundler:2.2.14
+bundle exec jekyll build
+
+# push
+cd _site
+git config user.email "kgerg13@gmail.com"
+git config user.name "Gergelj Kiš"
+git add --all
+git commit -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push origin gh-pages
